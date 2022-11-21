7ecf1eda3824b185ea23e3b10fc0d74f023ab8dd
Auliyarois <roizfatoni290@gmail.com>
2022-11-21 13:10:00 +0700

    foodnbev draft

---
+++ FinalProject/FoodnBev.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalProject
+{
+    /// <summary>
+    /// Interaction logic for FoodnBev.xaml
+    /// </summary>
+    public partial class FoodnBev : Page
+    {
+        public FoodnBev()
+        {
+            InitializeComponent();
+        }
+
+        private void minus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void plus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void minus_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void plus_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void minus_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void plus_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
