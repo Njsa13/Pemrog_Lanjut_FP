b57af965c53c454ab5df9d2947a8fce08ccd6e21
Auliyarois <roizfatoni290@gmail.com>
2022-11-21 12:10:56 +0700

    addbilling

--- FinalProject/AddBilling.xaml.cs
+++ FinalProject/AddBilling.xaml.cs
@@ -24,5 +24,10 @@ namespace FinalProject
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
